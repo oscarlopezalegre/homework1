9daa4b5da5a760b7f336afaf3728391f8112008d
Oscar Lopez Alegre <oscar.lopez.alegre@live.com>
2015-12-10 00:42:15 +0700

    tired

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Homework1
     
     
     config.serve_static_assets = true
-    #config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+    config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
     config.assets.compile = true
     
     
